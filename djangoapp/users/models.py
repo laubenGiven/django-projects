5dc7058a09b9a9103da8d39995d47726b8aa3d02
laubenGiven <mpairwelauben375@gmail.com>
2023-12-17 22:01:17 +0300

    model add

--- djangoapp/users/models.py
+++ djangoapp/users/models.py
@@ -2,6 +2,13 @@ from django.db import models
 
 # Create your models here.
 class Person(models.Model):
+    user_number = models.PositiveIntegerField(default=23)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
+    email = models.EmailField(default='alien@gmail.com',max_length=100)
+    field_of_profession = models.CharField(default='engineering',max_length=75)
+    salaray = models.FloatField()
+
+    def __str__(self) :
+        return f'user:{self.first_name} {self.last_name}'
 
--- djangoapp/users/views.py
+++ djangoapp/users/views.py
@@ -1,3 +1,12 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse 
+from .models import Person
 
 # Create your views here.
+def index(request):
+    return render(request,'users/index.html',{'users':Person.objects.all()})
+
+def view_user(request,id):
+    user = Person.objects.get(pk=id)
+    return HttpResponseRedirect(reverse(index))
